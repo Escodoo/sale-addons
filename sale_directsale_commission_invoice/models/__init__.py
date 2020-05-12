07401cefab0510c350bcf3b8c886f041febc083b
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-05-12 03:20:42 -0300

    Remove sale.order.line model

--- sale_directsale_commission_invoice/models/__init__.py
+++ sale_directsale_commission_invoice/models/__init__.py
@@ -1,4 +1,3 @@
-from . import sale_order_line
 from . import sale_order
 from . import account_invoice
 from . import product_template
--- sale_directsale_commission_invoice/models/sale_order_line.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2020 Marcel Savegnago - Escodoo
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from odoo import api, fields, models, _
-
-
-class SaleOrderLine(models.Model):
-
-    _inherit = 'sale.order.line'
