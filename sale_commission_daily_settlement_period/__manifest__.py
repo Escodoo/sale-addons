ce719a9a8c9957712f19d2641d40be1c95544bea
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-05-18 18:15:52 -0300

    [12.0][ADD]Enable daily settlement period

---
+++ sale_commission_daily_settlement_period/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sale Commission Daily Settlement Period',
+    'summary': """
+        This module enable daily settlement period for commission agents""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo,Odoo Community Association (OCA)',
+    'website': 'www.escodoo.com.br',
+    'depends': [
+        'sale_commission',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
---
+++ sale_commission_daily_settlement_period/models/res_partner.py
@@ -0,0 +1,12 @@
+# Copyright 2020 Marcel Savegnago - Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+
+
+class ResPartner(models.Model):
+
+    _inherit = 'res.partner'
+
+    settlement = fields.Selection(
+        selection_add=[('daily', 'Daily')])
