7adb14dad3e7ad510483cbf42156a4ddfdb4ccd5
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-08-12 12:56:41 -0300

    [ADD] sale_invoice_plan_editable

---
+++ sale_invoice_plan_editable/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Invoice Plan Editable",
+    "summary": """
+        This module allows you to change the invoice plan while
+        the invoice_plan line is not invoiced.""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/sale-addons",
+    "depends": ["sale_invoice_plan"],
+    "data": [
+        "views/sale_order.xml",
+    ],
+    "demo": [],
+}
