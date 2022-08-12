7c813a74c415a2d619e18ab859559538275d024a
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-08-12 13:33:54 -0300

    [MIG] sale_invoice_plan_editable: Migration to 12.0

--- sale_invoice_plan_editable/__manifest__.py
+++ sale_invoice_plan_editable/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
         This module allows you to change the invoice plan while
         the invoice_plan line is not invoiced.""",
-    "version": "14.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo",
     "website": "https://github.com/Escodoo/sale-addons",
